ca9b58cda9843d3eb220d5e7014c53df2fa58f39
modisagar <modi.sagar4u@gmail.com>
2016-09-15 18:32:03 +0530

    Day 2

--- CandidateCode/Program.cs
+++ CandidateCode/Program.cs
@@ -11,11 +11,30 @@ namespace CandidateCode
         static void Main(string[] args)
         {
             string stdin = Console.ReadLine();
-            output(stdin);
+            day2(stdin);
             
         }
 
-        private static void output(string stdin)
+        private static void day2(string str)
+        {
+            
+            if (!string.IsNullOrEmpty(str))
+            {
+                Int32 intValue;
+                float doubleValue;
+                
+                if (Int32.TryParse(str, out intValue))
+                    Console.WriteLine("This input is of type Integer.");
+                else if (float.TryParse(str, out doubleValue))
+                    Console.WriteLine("This input is of type Float.");
+                else if (str is string)
+                    Console.WriteLine("This input is of type String.");
+                else
+                    Console.WriteLine("This is something else."); 
+            }
+        }
+        
+        private static void day1(string stdin)
         {
             Console.WriteLine("Hello Techgig");
             Console.WriteLine(stdin);
