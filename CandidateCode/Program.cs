da0ade000c0bb895009232ef3b5585828c1f7f80
modisagar <modi.sagar4u@gmail.com>
2016-09-15 19:47:59 +0530

    Day 3 Fixes #1

--- CandidateCode/Program.cs
+++ CandidateCode/Program.cs
@@ -10,9 +10,50 @@ namespace CandidateCode
     {
         static void Main(string[] args)
         {
-            string stdin = Console.ReadLine();
-            day2(stdin);
+            double principle=0;
+            int interest=0;
+            int year=0;
+            int output=0;
+
+            //Console.WriteLine("Give principle amount:");
+            try
+            {
+                principle = double.Parse(Console.ReadLine().ToString());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Principle is not in correct format");
+                System.Environment.Exit(0);
+            }
+            //Console.WriteLine("Give Interest rate:");
+            try
+            {
+                interest = int.Parse(Console.ReadLine().ToString());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Interest is not in correct format");
+                System.Environment.Exit(0);
+            }
+            //Console.WriteLine("Give Year(s):");
+            try
+            {
+                year = int.Parse(Console.ReadLine().ToString());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Interest is not in correct format");
+                System.Environment.Exit(0);
+            }
+
+            output = day3(principle, interest, year);
+            Console.WriteLine(output);
             
+        }
+
+        private static int day3(double principle, int interest, int year)
+        {
+            return (int)Math.Round((principle * interest * year) / 100);
         }
 
         private static void day2(string str)
